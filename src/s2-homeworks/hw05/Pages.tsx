d93d685c23c6eb2abf671fbb8a17e141aab3c40f
esempe <steam.dota.return@gmail.com>
2024-06-06 21:11:39 +0300

    hw06 attempt 1

--- src/s2-homeworks/hw05/Pages.tsx
+++ src/s2-homeworks/hw05/Pages.tsx
@@ -16,20 +16,12 @@ function Pages() {
 		<div>
 			{/*Routes выбирает первый подходящий роут*/}
 			<Routes>
-				{/*роутинг будут писать студенты*/}
-				{/*в начале мы попадаем на страницу '/' и переходим сразу на страницу /pre-junior*/}
-				{/*<Route ...*/}
 				<Route path="/" element={<Navigate to={"/pre-junior"} />} />
 				<Route path="/pre-junior" element={<PreJunior />} />
 				<Route path="/junior" element={<Junior />} />
 				<Route path="/junior-plus" element={<JuniorPlus />} />
-				{/*роуты для /pre-junior, /junior, /junior-plus*/}
-				{/*<Route ...*/}
-				{/*<Route ...*/}
-				{/*<Route ...*/}
+
 				<Route path="/*" element={<Error404 />} />
-				{/*роут для несуществующей страницы должен отрисовать <Error404 />*/}
-				{/*<Route ...*/}
 			</Routes>
 		</div>
 	);
