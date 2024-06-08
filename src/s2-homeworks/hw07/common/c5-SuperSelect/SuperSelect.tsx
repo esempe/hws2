fcf5bf37be699f3f7d2b1e4cbfbff3537ba03bad
esempe <steam.dota.return@gmail.com>
2024-06-08 21:58:49 +0300

    hw07 attempt 1

--- src/s2-homeworks/hw07/common/c5-SuperSelect/SuperSelect.tsx
+++ src/s2-homeworks/hw07/common/c5-SuperSelect/SuperSelect.tsx
@@ -1,55 +1,70 @@
 import React, {
-    SelectHTMLAttributes,
-    DetailedHTMLProps,
-    ChangeEvent,
-} from 'react'
-import s from './SuperSelect.module.css'
+	SelectHTMLAttributes,
+	DetailedHTMLProps,
+	ChangeEvent,
+} from "react";
+import s from "./SuperSelect.module.css";
 
 type DefaultSelectPropsType = DetailedHTMLProps<
-    SelectHTMLAttributes<HTMLSelectElement>,
-    HTMLSelectElement
->
+	SelectHTMLAttributes<HTMLSelectElement>,
+	HTMLSelectElement
+>;
 
 type SuperSelectPropsType = DefaultSelectPropsType & {
-    options?: any[]
-    onChangeOption?: (option: any) => void
-}
+	options?: any[];
+	onChangeOption?: (option: any) => void;
+};
 
 const SuperSelect: React.FC<SuperSelectPropsType> = ({
-    options,
-    className,
-    onChange,
-    onChangeOption,
-    ...restProps
+	value,
+	options,
+	className,
+	onChange,
+	onChangeOption,
+	...restProps
 }) => {
-    const mappedOptions: any[] = options
-        ? options.map((o) => (
-              <option
-                  id={'hw7-option-' + o.id}
-                  className={s.option}
-                  key={o.id}
-                  value={o.id}
-              >
-                  {o.value}
-              </option>
-          ))
-        : [] // map options with key
+	const mappedOptions: any[] = options
+		? options.map((o) => (
+				<option
+					id={"hw7-option-" + o.id}
+					className={s.option}
+					key={o.id}
+					value={o.id}
+				>
+					{o.value}
+				</option>
+		  ))
+		: []; // map options with key
 
-    const onChangeCallback = (e: ChangeEvent<HTMLSelectElement>) => {
-        // делают студенты
-    }
+	const onChangeCallback = (e: ChangeEvent<HTMLSelectElement>) => {
+		// делают студенты
+		onChangeOption?.(e.currentTarget.value);
+	};
 
-    const finalSelectClassName = s.select + (className ? ' ' + className : '')
+	const finalSelectClassName = s.select + (className ? " " + className : "");
+	const getValue = (s: string): number => {
+		switch (s) {
+			case "x":
+				return 1;
+			case "y":
+				return 2;
+			case "z":
+				return 3;
 
-    return (
-        <select
-            className={finalSelectClassName}
-            onChange={onChangeCallback}
-            {...restProps}
-        >
-            {mappedOptions}
-        </select>
-    )
-}
+			default:
+				return 1;
+		}
+	};
+	return (
+		<select
+			className={finalSelectClassName}
+			onChange={onChangeCallback}
+			value={getValue(value as string)}
+			{...restProps}
+		>
+			{mappedOptions}
+		</select>
+	);
+};
 
-export default SuperSelect
+export default SuperSelect;
