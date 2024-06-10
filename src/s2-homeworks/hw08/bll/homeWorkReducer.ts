65154cd1f7d688e58d3195aa8ca5a861ab774f4f
esempe <steam.dota.return@gmail.com>
2024-06-10 19:28:17 +0300

    hw08 att 1

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -1,20 +1,40 @@
-import {UserType} from '../HW8'
+import { UserType } from "../HW8";
 
 type ActionType =
-    | { type: 'sort'; payload: 'up' | 'down' }
-    | { type: 'check'; payload: number }
+	| { type: "sort"; payload: "up" | "down" }
+	| { type: "check"; payload: number };
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
-    switch (action.type) {
-        case 'sort': { // by name
+export const homeWorkReducer = (
+	state: UserType[],
+	action: ActionType
+): UserType[] => {
+	// need to fix any
+	switch (action.type) {
+		case "sort": {
+			// by name
 
-            return state // need to fix
-        }
-        case 'check': {
+			const newState = [...state];
+			switch (action.payload) {
+				case "up": {
+					return newState.sort((a, b) =>
+						a.name.toLowerCase().localeCompare(b.name.toLowerCase())
+					); // need to fix
+				}
+				case "down": {
+					return newState.sort((a, b) =>
+						b.name.toLowerCase().localeCompare(a.name.toLowerCase())
+					); // need to fix
+				}
+				default:
+					return state;
+			}
+		}
+		case "check": {
+			// const newState = [...state];
 
-            return state // need to fix
-        }
-        default:
-            return state
-    }
-}
+			return state.filter((u) => u.age >= 18); // need to fix
+		}
+		default:
+			return state;
+	}
+};
