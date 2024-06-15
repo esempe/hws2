320d6cf09e384c3015a9ad0fac8c9f5244bbdee8
esempe <steam.dota.return@gmail.com>
2024-06-15 22:15:33 +0300

    hw10 att 1

--- src/s2-homeworks/hw10/bll/loadingReducer.ts
+++ src/s2-homeworks/hw10/bll/loadingReducer.ts
@@ -1,22 +1,38 @@
+import { Dispatch } from "redux";
+
 const initState = {
-    isLoading: false,
-}
+	isLoading: false,
+};
 
-export const loadingReducer = (state = initState, action: any): any => { // fix any
-    switch (action.type) {
-        // пишет студент  // need to fix
-
-        default:
-            return state
-    }
-}
+type ActionType = LoadingActionType;
+export const loadingReducer = (
+	state = initState,
+	action: ActionType
+): { isLoading: boolean } => {
+	// fix any
+	switch (action.type) {
+		// пишет студент  // need to fix
+		case "CHANGE_LOADING": {
+			return { isLoading: action.isLoading };
+		}
+		default:
+			return state;
+	}
+};
 
 type LoadingActionType = {
-    type: 'CHANGE_LOADING'
-    isLoading: boolean
-}
+	type: "CHANGE_LOADING";
+	isLoading: boolean;
+};
 
 export const loadingAC = (isLoading: boolean): LoadingActionType => ({
-    type: 'CHANGE_LOADING',
-    isLoading,
-})
+	type: "CHANGE_LOADING",
+	isLoading,
+});
+
+export const loadingTC =
+	(ms: number, isLoading: boolean) => (dispatch: Dispatch) => {
+		return setTimeout(() => {
+			dispatch(loadingAC(isLoading));
+		}, ms);
+	};
--- src/s2-homeworks/hw10/bll/store.ts
+++ src/s2-homeworks/hw10/bll/store.ts
@@ -1,17 +1,30 @@
-import { loadingReducer } from './loadingReducer'
-import { combineReducers, legacy_createStore } from 'redux'
-import { themeReducer } from '../../hw12/bll/themeReducer'
+import { loadingReducer } from "./loadingReducer";
+import {
+	AnyAction,
+	applyMiddleware,
+	combineReducers,
+	legacy_createStore,
+} from "redux";
+import { themeReducer } from "../../hw12/bll/themeReducer";
+import { useDispatch } from "react-redux";
+import { ThunkDispatch, thunk } from "redux-thunk";
 
 const reducers = combineReducers({
-    loading: loadingReducer, // hw10
-    theme: themeReducer, // hw12
-})
+	loading: loadingReducer, // hw10
+	theme: themeReducer, // hw12
+});
 
-const store = legacy_createStore(reducers)
+const store = legacy_createStore(reducers, applyMiddleware(thunk));
 
-export default store
+export default store;
 
-export type AppStoreType = ReturnType<typeof reducers>
+export type AppThunkDispatch = ThunkDispatch<AppRootStateType, any, AnyAction>;
+
+export type AppStoreType = ReturnType<typeof reducers>;
+
+export type AppRootStateType = ReturnType<typeof reducers>;
+
+export const useAppDispatch = () => useDispatch<AppThunkDispatch>();
 
 // @ts-ignore
-window.store = store // for dev // для того чтобы автотесты видели состояние данных
+window.store = store; // for dev // для того чтобы автотесты видели состояние данных
