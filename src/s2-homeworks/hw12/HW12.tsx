a30d4c2bcc4a66dfabaec2e45dfd7422766568a4
esempe <steam.dota.return@gmail.com>
2024-06-18 15:41:06 +0300

    hw 12 att1

--- src/s2-homeworks/hw12/HW12.tsx
+++ src/s2-homeworks/hw12/HW12.tsx
@@ -1,51 +1,54 @@
-import React, {useEffect} from 'react'
-import s from './HW12.module.css'
-import s2 from '../../s1-main/App.module.css'
-import SuperSelect from '../hw07/common/c5-SuperSelect/SuperSelect'
-import {useDispatch, useSelector} from 'react-redux'
-import {changeThemeId} from './bll/themeReducer'
+import { useEffect } from "react";
+import { useSelector } from "react-redux";
+import s2 from "../../s1-main/App.module.css";
+import SuperSelect from "../hw07/common/c5-SuperSelect/SuperSelect";
+import { AppStoreType, useAppDispatch } from "../hw10/bll/store";
+import s from "./HW12.module.css";
+import { ThemeStateType, changeThemeId } from "./bll/themeReducer";
 
 /*
-* 1 - в файле themeReducer.ts написать нужные типы вместо any, дописать редьюсер
-* 2 - получить themeId из редакса
-* 3 - дописать тип и логику функции change
-* 4 - передать пропсы в SuperSelect
-* */
-
+ * 1 - в файле themeReducer.ts написать нужные типы вместо any, дописать редьюсер
+ * 2 - получить themeId из редакса
+ * 3 - дописать тип и логику функции change
+ * 4 - передать пропсы в SuperSelect
+ * */
+export type Option = { id: number; value: string | number };
 const themes = [
-    {id: 1, value: 'light'},
-    {id: 2, value: 'blue'},
-    {id: 3, value: 'dark'},
-]
+	{ id: 1, value: "light" },
+	{ id: 2, value: "blue" },
+	{ id: 3, value: "dark" },
+];
 
 const HW12 = () => {
-    // взять ид темы из редакса
-    const themeId = 1
+	// взять ид темы из редакса
+	const themeId = useSelector<AppStoreType, number>((s) => s.theme.themeId);
+	const dispatch = useAppDispatch();
+	const change = (value: Option) => {
+		dispatch(changeThemeId(value.id));
+		// дописать функцию
+	};
+	console.log(themeId);
+	useEffect(() => {
+		document.documentElement.dataset.theme = themeId + "";
+	}, [themeId]);
 
-    const change = (id: any) => { // дописать функцию
+	return (
+		<div id={"hw12"}>
+			<div id={"hw12-text"} className={s2.hwTitle}>
+				Homework #12
+			</div>
 
-    }
+			<div className={s2.hw}>
+				<SuperSelect
+					id={"hw12-select-theme"}
+					className={s.select}
+					value={themeId}
+					onChangeOption={change}
+					options={themes}
+				/>
+			</div>
+		</div>
+	);
+};
 
-    useEffect(() => {
-        document.documentElement.dataset.theme = themeId + ''
-    }, [themeId])
-
-    return (
-        <div id={'hw12'}>
-            <div id={'hw12-text'} className={s2.hwTitle}>
-                Homework #12
-            </div>
-
-            <div className={s2.hw}>
-                <SuperSelect
-                    id={'hw12-select-theme'}
-                    className={s.select}
-                    // сделать переключение тем
-
-                />
-            </div>
-        </div>
-    )
-}
-
-export default HW12
+export default HW12;
--- src/s2-homeworks/hw12/bll/themeReducer.ts
+++ src/s2-homeworks/hw12/bll/themeReducer.ts
@@ -1,14 +1,22 @@
-const initState = {
-    themeId: 1,
-}
+const initState: ThemeStateType = {
+	themeId: 1,
+};
+export type ThemeStateType = {
+	themeId: number;
+};
+export const themeReducer = (state = initState, action: ActionType): any => {
+	// fix any
+	switch (action.type) {
+		// дописать
+		case "SET_THEME_ID": {
+			return { ...state, themeId: action.id };
+		}
+		default:
+			return state;
+	}
+};
 
-export const themeReducer = (state = initState, action: any): any => { // fix any
-    switch (action.type) {
-        // дописать
+export const changeThemeId = (id: number) =>
+	({ type: "SET_THEME_ID", id } as const); // fix any
 
-        default:
-            return state
-    }
-}
-
-export const changeThemeId = (id: number): any => ({ type: 'SET_THEME_ID', id }) // fix any
+type ActionType = ReturnType<typeof changeThemeId>;
